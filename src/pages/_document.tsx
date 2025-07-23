7c59dccd27128b1aaac91a123c4c465d0e75b2b7
airaarima <airaarima.dev@gmail.com>
2025-07-22 22:55:30 -0300

    feat: configura varíaveis e fontes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
+    <Html lang="pt-br">
+      <Head>
+        <title>Mitiele Lopes</title>
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
