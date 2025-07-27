54e535e2bf7afc8163a07eaf7b71dbb3dbc95caa
airaarima <airaarima.dev@gmail.com>
2025-07-27 16:50:04 -0300

    feat: implementa menu hamburguer e ajusta responsividade

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,35 +2,45 @@ import React from "react";
 import { navLinks } from "../../constant/constant";
 import Link from "next/link";
 import Image from "next/image";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import MenuBar from "./MenuBar";
 
 const Navbar = () => {
   return (
-    <nav className="w-full h-[10vh] z-[100] border-b border-b-[#2B2B2B] pl-20 pr-20">
+    <nav className="w-full max-lg:pl-10 max-lg:pr-10 h-[10vh] z-[100] border-b border-b-[#2B2B2B] pl-20 pr-20">
       <div className="flex items-center h-full justify-between">
         {/* Logo */}
-        <div className="flex items-center ">
+        <div className="flex items-center flex-none">
+          <div className="relative w-12 h-12 max-lg:w-10 max-lg:h-10">
             <Image
-                aria-hidden
-                src="/navbar/law.png"
-                alt="Logo icon"
-                width={48}
-                height={48}
-              />
-              <h1 className="ml-[10px] text-2xl font-[--font-playfair] font-semibold ">Dra. Mitiele Lopes</h1>
+              src="/navbar/law.png"
+              alt="Logo icon"
+              fill
+              aria-hidden
+            />
+          </div>
+          <h1 className="ml-[10px] text-2xl font-[--font-playfair] font-semibold max-lg:text-xl">
+            Dra. Mitiele Lopes
+          </h1>
         </div>
-        {/* NavLinks */}
-        <div className="text-sm flex items-center space-x-10">
-          {navLinks.map((link) => {
-            return (
-              <Link href={link.url} key={link.id} className="">
-                <p>{link.label}</p>
-              </Link>
-            );
-          })}
+        <div className="hidden lg:flex items-center space-x-10 flex-grow">
+          {/* NavLinks */}
+          <div className="flex justify-center w-full items-center space-x-10 text-sm">
+            {navLinks.map((link) => {
+              return (
+                <Link href={link.url} key={link.id} className="">
+                  <p>{link.label}</p>
+                </Link>
+              );
+            })}
+          </div>
+          {/* Button */}
+          <Button variant="outline">Consulta Gratuita</Button>
         </div>
-        {/* Button */}
-        <Button variant="outline" h>Consulta Gratuita</Button>
+        {/* Menu Mobile*/}
+        <div className="hidden max-lg:flex">
+          <MenuBar navLinks={navLinks} />
+        </div>
       </div>
     </nav>
   );
