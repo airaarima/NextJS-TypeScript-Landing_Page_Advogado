67643bc0147106d7d5910d4a9c9297c648734fd8
airaarima <airaarima.dev@gmail.com>
2025-08-04 22:11:21 -0300

    feat: adiciona icon na aba do navegador

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <link rel="icon" href="/navbar/law.png" />
         <title>Dra. Mitiele Lopes</title>
       </Head>
       <Component {...pageProps} />
