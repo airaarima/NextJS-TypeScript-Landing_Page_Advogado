a39629a37f08e56e07fcde3590b5ec89f0732f79
airaarima <airaarima.dev@gmail.com>
2025-08-06 21:27:17 -0300

    feat: implementa formulário

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -10,7 +10,7 @@ const Contact = () => {
       className="bg-[var(--color-primary)] space-y-8 justify-between items-center w-full max-lg:pl-10 max-lg:pr-10 pl-20 pr-20 text-[var(--color-text-primary)] pb-7 pt-8"
     >
       <div className="flex justify-between gap-24">
-        <div className="space-y-5">
+        <div className="space-y-5 xl:flex xl:flex-col w-full xl:w-auto">
           <h3 className="text-3xl max-lg:text-2xl">Entre em Contato</h3>
           <p className="text-xl max-lg:text-lg">
             Precisa de ajuda jurídica? Entre em contato e agende sua consulta
@@ -18,7 +18,7 @@ const Contact = () => {
           </p>
           <Form />
         </div>
-        <div className="max-lg:hidden">
+        <div className="max-xl:hidden flex-shrink-0">
             <img src="/contact/adv-contact.png" alt="" className="rounded "/>
         </div>
       </div>
