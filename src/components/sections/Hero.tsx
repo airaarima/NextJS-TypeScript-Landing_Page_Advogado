0c7e618248e5d952d387dc878a019c2eb0f5eb34
airaarima <airaarima.dev@gmail.com>
2025-08-11 19:31:42 -0300

    feat: adiciona link para o whatsapp

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -5,7 +5,10 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section id="home" className="mt-4 flex justify-center xl:justify-between w-full max-lg:pl-10 max-lg:pr-10 pl-20 pr-20 max-sm:min-h-[30vh] min-h-[20vh] lg:min-h-[30vh]">
+    <section
+      id="home"
+      className="mt-4 flex justify-center xl:justify-between w-full max-lg:pl-10 max-lg:pr-10 pl-20 pr-20 max-sm:min-h-[30vh] min-h-[20vh] lg:min-h-[30vh]"
+    >
       <div className="flex flex-col justify-center md:pr-4">
         {/* Info */}
         <div className="h-full xl:w-full flex flex-col justify-center max-xl:items-center">
@@ -22,20 +25,29 @@ const Hero = () => {
 
         {/* Botões */}
         <div className="flex justify-center md:space-x-6 max-md:space-x-2">
-          <Button variant="outline" className="max-sm:w-[145px] w-[168px] max-sm:text-xs bg-[var(--color-accent-dark)]">
-            <Phone/>
+          <Button
+            variant="outline"
+            className="max-sm:w-[145px] w-[168px] max-sm:text-xs bg-[var(--color-accent-dark)]"
+          >
+            <Phone />
             <a href="#contact">Consulta Gratuita</a>
-            </Button>
-          <Button className="max-sm:w-[145px] w-[168px] max-sm:text-xs bg-[var(--color-accent-dark)] hover:cursor-pointer">
+          </Button>
+          <Button asChild className="max-sm:w-[145px] w-[168px] max-sm:text-xs bg-[var(--color-accent-dark)] hover:cursor-pointer">
+            <a
+              href="https://wa.me/5551993426635?text=Ol%C3%A1%2C%20gostaria%20de%20agendar%20um%20hor%C3%A1rio%20para%20conversarmos%20sobre%20meu%20caso%21"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
             <Image
-            src="/hero/whatsapp.png"
+              src="/hero/whatsapp.png"
               alt="Logo icon"
               width={16}
               height={16}
               aria-hidden
             />
             WhatsApp
-            </Button>
+            </a>
+          </Button>
         </div>
       </div>
 
